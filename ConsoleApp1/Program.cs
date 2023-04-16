11f5d21d5db080655b4bb218ee5426eaf9e83968
Мейлис <@DESKTOP-OU1N220>
2023-04-16 20:34:57 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Как Вас зовут?");
+            string name = Console.ReadLine();
+            Console.WriteLine("Сколько Вам лет?");
+            string age = Console.ReadLine();
+            Greet(name, age);
+            Console.WriteLine("Рад был познакомиться!");
+        }
+
+        static void Greet(string name, string age)
+        {
+            Console.WriteLine("Здравствуйте, {0}", name);
+            Console.WriteLine("Мне сообщили, что Вам {0}", age);
+        }
+    }
+}
